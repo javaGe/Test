4f8babc9387c6780534fdb1bcf5c447479ead06a
javaGe <15813081353@163.com>
2018-11-06 13:33:59 +0800

    自动化框架测试

--- tendao/common/BrowserStart.py
+++ tendao/common/BrowserStart.py
@@ -4,21 +4,32 @@ import time
 from test.tendao.common.ReadConfig import getConfigInfo
 from selenium import webdriver
 from test.tendao.common.CapPicture import capPicture
+from test.tendao.common.LogTool import getLogger
 '''
 根据不同的浏览器名称判断来启动不同的浏览器驱动
 '''
+
+# 获取日志对象
+log = getLogger('BrowserStart')
 
 # 获取浏览器名称
 browser_name = getConfigInfo('browser','BrowserName')
 # 获取浏览器请求的url
 url = getConfigInfo('browser','Url')
+
 if browser_name == 'Chrome':
+    log.info('启动Chrome浏览器')
     driver = webdriver.Chrome()
+
+    log.info('请求网页：'+url)
     driver.get(url)
+
 if browser_name == 'IE':
     driver = webdriver.Ie()
     driver.get(url)
+# 进行截图
 capPicture(driver)
 
 time.sleep(3)
 driver.quit()
+log.info('关闭浏览器')
---
+++ tendao/common/LogTool.py
@@ -0,0 +1,39 @@
+#coding=utf-8
+import logging # 导入日志模块
+import os
+import time
+
+def getLogger(name):
+    # 创建一个log对象
+    logger = logging.getLogger(name)
+    # 设置日子打印的级别
+    logger.setLevel(logging.INFO)
+
+    # 日志名称
+    log_name = name + '-' + time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
+
+    # 创建日志句柄，日志文件存储的位置
+    log_file_path = os.path.dirname(os.path.abspath('.'))+'\\log\\'+log_name+'.log'
+    fileh = logging.FileHandler(log_file_path)
+    # 设置日志级别
+    fileh.setLevel(logging.INFO)
+
+    # 创建在终端也输出日志
+    consoleh = logging.StreamHandler()
+    consoleh.setLevel(logging.INFO)
+
+
+    # 设置日志打印的格式
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fileh.setFormatter(formatter)
+    consoleh.setFormatter(formatter)
+
+    # 创建添加日志的句柄
+    logger.addHandler(fileh)
+    logger.addHandler(consoleh)
+
+    return logger
+
+    # logger.info('hello python')
+
+# getLogger('test')
